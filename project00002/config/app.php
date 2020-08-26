3ad99285701374964299104f4535e075300f674e
yazdi <aminashofteh@gmail.com>
2020-08-26 19:54:03 +0430

    mvc config created

--- project00002/config/app.php
+++ project00002/config/app.php
@@ -1,1 +1,18 @@
 <?php
+	// define some CONST for use in program
+	define('APP_TITLE', 'mvc project');
+	define('BASE_URL', '/My/topLearn_phpMVC/project00002');
+	define('BASE_DIR', realpath((__DIR__)."/../"));
+	$temporary = str_ireplace(BASE_URL,'',explode('?',$_SERVER['REQUEST_URI'])[0]);
+	$temporary === "/" ? $temporary = "" : $temporary = substr($temporary,1);
+	define('CURREN_ROUTE', $temporary);
+	global $routes;
+	$routes = [
+	'get' => [],
+	'post' => [],
+	'put' => [],
+	'delete' => [],
+	];
+	
+	print_r($routes);
+?>
